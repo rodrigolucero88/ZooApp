3fc7900425465741d3baa422406a184455526e51
rodrigolucero88 <141675408+rodrigolucero88@users.noreply.github.com>
2023-08-08 14:44:46 -0300

    Initial commit

---
+++ src/zooapp/ZooApp.java
@@ -0,0 +1,27 @@
+
+package zooapp;
+
+/**
+ Un zoológico necesita cargar los animales q posee en el sector norte de este, allí se encuentran 6 especies, 
+  todos animales comparten algunas características como, peso, nombre, sexo,edad , y algunos particulares como
+  el elefante el largo de la trompa, de la jirafa el largo de cuello, el rinoceronte el largo de su cuerno mayor, 
+  del cocodrilo su longitud, de mono la especie y de aves el color y la especie. Cada animal tiene un código único de identificación.
+  También se desea agregar en un registro los empleados y el turno en q trabajaron,
+  el zoo cuenta con 4 turnos y 4 empleados con turnos rotativos.
+Además se debe poder consultar cuanta comida requiere cada especie. El cálculo se basa según su peso, el elefante come 4 veces su peso,
+ el rinoceronte 3 veces su peso, la jirafa la mitad de su peso, el mono un cuarto de su peso las aves un tercio y el cocodrilo su mismo peso.
+El sistema debe tener un menú por el cual podamos agregar un animal nuevo, un empleado nuevo, eliminar un animal y un empleado,
+ consultar la lista de animales, completo o por especie, consultar los empleados y sus turnos. 
+ Consultar cuanto alimento se le debe dar a cada animal qué se desee alimentar.
+También el sistema debe permitir el logueo al sistema de los empleados antes de realizar cualquier tarea.
+ */
+public class ZooApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
