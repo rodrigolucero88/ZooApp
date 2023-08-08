5b872049648098901b4edf1efa54796aa678effe
lean2110 <leandrocuello01@gmail.com>
2023-08-08 17:14:52 -0300

    Update Empleado.java

--- src/zooapp/Empleado.java
+++ src/zooapp/Empleado.java
@@ -9,5 +9,28 @@ package zooapp;
  * @author Leandro
  */
 public class Empleado {
+    private String nombre;
+    private String Turno;
+
+    public Empleado(String nombre, String Turno) {
+        this.nombre = nombre;
+        this.Turno = Turno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTurno() {
+        return Turno;
+    }
+
+    public void setTurno(String Turno) {
+        this.Turno = Turno;
+    }
     
 }
